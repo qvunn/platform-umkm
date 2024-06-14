fb22bd24635ad05e934f385ac9db337b47b309f2
qvunn <149144629+qvunn@users.noreply.github.com>
2024-06-14 23:19:34 +0700

    Icon
    Make icon and change name logo

--- resources/views/include/left-sidebar.blade.php
+++ resources/views/include/left-sidebar.blade.php
@@ -3,26 +3,31 @@
         <ul class="nav nav-link-secondary flex-column fw-bold gap-2">
             <li class="nav-item">
                 <a class="nav-link text-primary" href="#">
+                    <i class="bi bi-house"></i>
                     <span>Home</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link text-white" href="/">
+                    <i class="bi bi-book"></i>
                     <span>Feeds</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link text-white" href="/post">
+                    <i class="bi bi-plus-circle"></i>
                     <span>Post</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link text-white" href="#">
+                    <i class="bi bi-bookmarks"></i>
                     <span>Saved</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link text-white" href="#">
+                    <i class="bi bi-person-fill"></i>
                     <span>Profile</span>
                 </a>
             </li>
